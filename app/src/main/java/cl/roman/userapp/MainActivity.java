4e07660a2cb132fe8dc73bf7e0ae66ef51362d28
Roman <roman.gajardo@gmail.com>
2020-09-09 18:11:23 -0300

    Adapter con Glide
    Obtención de valores desde un JSON

--- app/src/main/java/cl/roman/userapp/MainActivity.java
+++ app/src/main/java/cl/roman/userapp/MainActivity.java
@@ -1,6 +1,8 @@
 package cl.roman.userapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -12,11 +14,16 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import cl.roman.userapp.adapter.AdapterUser;
+import cl.roman.userapp.model.User;
 import cz.msebera.android.httpclient.Header;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String URL="https://randomuser.me/api/?results=5";
+    private static final String URL="https://randomuser.me/api/?results=50";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +55,34 @@ public class MainActivity extends AppCompatActivity {
         try {
             JSONObject root = new JSONObject(data);
             JSONArray results = root.getJSONArray("results");
+
+            List<User> list = new ArrayList<>();
+
+            for(int i = 0; i<results.length();i++) {
+                JSONObject user1 = results.getJSONObject(i);
+                String email = user1.getString("email");
+                String phone = user1.getString("phone");
+
+                JSONObject name = user1.getJSONObject("name");
+                String first = name.getString("first");
+                String last = name.getString("last");
+
+                JSONObject picture = user1.getJSONObject("picture");
+                String thumbnail = picture.getString("thumbnail");
+
+                User user = new User(first, last,phone,email,thumbnail);
+                list.add(user);
+            }
+
+            //caargar el recycler
+            RecyclerView rc = findViewById(R.id.rc_user);
+            AdapterUser ad = new AdapterUser(this,list,R.layout.item_user);
+            LinearLayoutManager lm = new LinearLayoutManager(this);
+            lm.setOrientation(RecyclerView.VERTICAL);
+
+            rc.setLayoutManager(lm);
+            rc.setAdapter(ad);
+
 
 
         } catch (JSONException e) {
